878d8f179463e26d621f5a38e664ceef52968451
kimberly lin <kimberly.lin10@gmail.com>
2021-09-28 16:03:05 -0700

    wrote makeChains method for Markov class

--- markov.js
+++ markov.js
@@ -7,6 +7,7 @@ class MarkovMachine {
 
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
+    this.words = words;
     // MORE CODE HERE
   }
 
@@ -16,13 +17,32 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // MORE CODE HERE
+    // create empty object
+    // loop through the words array
+    // put word as key, add word+1 to values
+    let chains = {};
+    //option 1
+    for (let i = 0; i < this.words.length; i++) {
+      let nextVal = this.words[i + 1] ? this.words[i + 1] : null;
+      //if key doesn't already exist
+      if (!(chains[this.words[i]])) {
+        //create key and value in an array
+        chains[this.words[i]] = [];
+        chains[this.words[i]].push(nextVal)
+      } else {
+        // if key already exists, append val to array
+        chains[this.words[i]].push(nextVal);
+      }
+    }
+    return chains;
   }
-
 
   /** return random text from chains */
 
   getText(numWords = 100) {
     // MORE CODE HERE
+    // pick random starting point
+    // loops until we hit null
+    // output the chain
   }
 }
